61dc63cdce6409549993c2f513a491452f01bac6
grmvvr <gerome.vivar@gmail.com>
2025-01-08 17:18:43 +0100

    docs: initial files for rtd portfolio

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'GV'
+# license = 'This work is licensed under CC BY-NC-SA 4.0 '
+copyright = '2025, gerome vivar'
+author = 'gerome vivar'
+# release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_logo = "_static/logo.png"
+html_favicon = "_static/logo.png"
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,19 @@
+.. portfolio documentation master file, created by
+   sphinx-quickstart on Wed Jan  8 16:38:55 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+My Portfolio
+=======================
+
+.. Add your content using ``reStructuredText`` syntax. See the
+.. `reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+.. documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   markdown/p1.md
+   markdown/p2.md
